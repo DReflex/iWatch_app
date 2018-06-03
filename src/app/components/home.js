e30fee0d3888a7c3765560cef5da66a7ede2725c
DReflex <tkd.vatra@hotmail.com>
2018-06-03 15:11:23 +0200

    basic skelet of a react app

---
+++ src/app/components/home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class Home extends React.Component{
+  render(){
+    return(
+      <div id="content">
+        <h1>Hello world</h1>
+      </div>
+    )
+  }
+}
+export default Home
---
+++ src/reducers/index.js
@@ -0,0 +1,8 @@
+import { combineReducers } from 'redux'
+import State from './state'
+
+const iWatch = combineReducers({
+  State
+})
+
+export default iWatch
